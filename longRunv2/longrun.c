dbc4d4ec7f204ac736e1c078c0e3e6cd306665fb
toethan777 <123092841+toethan777@users.noreply.github.com>
2023-09-18 22:20:12 -0400

    Add files via upload

--- longRunv2/longrun.c
+++ longRunv2/longrun.c
@@ -4,8 +4,8 @@ int main(int argc, char* argv[]){
 	//initialize variables
 	char currentChar ='\0', prevChar = '\0';
 	int currentRunLength=0, maxHrun=0, maxTrun=0;
+	//initialize prevChar with stdin
 	prevChar = getc(stdin);
-	printf("P:%c\n", prevChar);
 
 	while (!feof(stdin)){
 		//while not at the end of a file...
@@ -13,27 +13,43 @@ int main(int argc, char* argv[]){
 
 		if (currentChar !='T' && currentChar !='H'){
 			continue;
+			//skips over characters other than T and H
+			//given something like two X's in a row,
+			//it would not count them
+			//nor would it set the current character to the previous character
 		}
 
 		if (currentChar == prevChar && currentChar == 'H'){
+			currentRunLength++;
+			//if the previous character is equal to the current character
+			//and the current character is H
+			//add to currentRunLength
 			if (currentRunLength > maxHrun){
+				//also if the current Run length exceeds the maxHrun length
+				//add to max runHrun
 				maxHrun++;
-				currentRunLength++;
 			}
 		}
 
 		if (currentChar == prevChar && currentChar == 'T'){
+			currentRunLength++;
+			//if the previous character is equal to the current character
+			//and the current character is T
+			//add to currentRunLength
 			if (currentRunLength > maxTrun){
+				//also if the current Run length exceeds the maxTrun length
+				//add to max runTrun
 				maxTrun++;
-				currentRunLength++;
 			}
 		}
 
 		if (currentChar != prevChar){
+			//if the previous character is not equal to the current character,
+			//set the current length to one
 			currentRunLength = 1;
 		}
 		prevChar = currentChar;
-		printf("P:%c\n", prevChar);
+		//we set the previous character to the current character for the next loop
 	}	
 
 	if (maxTrun == maxHrun){
